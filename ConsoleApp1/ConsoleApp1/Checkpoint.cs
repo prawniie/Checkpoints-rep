38f54215800a033e3139317938cc50df164a5b08
prawniie <rebecka.ls.carlsson@gmail.com>
2019-01-04 07:26:25 +0100

    Initial commit

---
+++ ConsoleApp1/ConsoleApp1/Checkpoint.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Checkpoint
+    {
+
+        public void Checkpoint1()
+        {
+            //Checkpoint1
+        }
+
+        public void Checkpoint2()
+        {
+            //Checkpoint2
+        }
+
+    }
+}
